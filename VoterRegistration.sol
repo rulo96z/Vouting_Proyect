a0aad4082ce7a3109fb2716838814acc383ce698
Deep <webster.dpatel@gmail.com>
2022-03-01 15:42:31 -0800

    updated Voting.sol compile conflict

--- VoterRegistration.sol
+++ VoterRegistration.sol
@@ -2,9 +2,8 @@ pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract VoterRegistrationCard is ERC721, IERC721Receiver {
+contract VoterRegistrationCard is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -12,19 +11,10 @@ contract VoterRegistrationCard is ERC721, IERC721Receiver {
 
     }
 
-    function registerVoter(address voter, string memory tokenURI) public returns (uint256){
+    function registerVoter() public returns (uint256){
         _tokenIds.increment();
         uint256 newVoterId = _tokenIds.current();
-        _mint(voter, newVoterId);
-        _setTokenURI(newVoterId, tokenURI);
-
+        _mint(msg.sender, newVoterId);
         return newVoterId;
-    }
-    
-    /**
-     * Always returns `IERC721Receiver.onERC721Received.selector`.
-     */
-    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
-        return this.onERC721Received.selector;
     }
 }
--- Voting.sol
+++ Voting.sol
@@ -1,6 +1,16 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: MIT
+pragma solidity  >= 0.4.22 < 0.8.0;
 
-contract Voting{
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+
+contract Voting is ERC721, ERC20, ERC20Detailed, ERC20Mintable{
+        
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     address payable owner;
 
@@ -12,19 +22,44 @@ contract Voting{
     
     uint candidateCount;
     mapping(uint => candidate) public candidates; 
-    mapping(address => address) public voterRegistrationCard; //maps voterAddress to VRC token
+    mapping(address => uint256) public voterRegistrationCard; //maps voterAddress to VRC token
     mapping(address => bool) public registered; //voter is registered or not
     mapping(address => bool) public voted; //voter has voted or not
-    address [] registrations; //array of voter addresses
+    address [] private registrations; //array of voter addresses
 
     event eventVote(uint indexed candidateID);
     
-    modifier restricted(){
+    modifier restricted {
         require(msg.sender == owner, "You do not have authorization!");
+        _;
     }
 
-    constructor() public{
+    constructor(address _token) ERC721("VoterRegistrationCard", "VRC") ERC20Detailed("VoteToken", "VOTE", 18) {
+        mint(msg.sender, 1000000000);
         owner = msg.sender;
+    }
+
+    function registerVoter() public returns (uint256){
+        require(!registered[msg.sender], "You have already registered to vote!");
+
+        _tokenIds.increment();
+        uint256 newVoterId = _tokenIds.current();
+        _mint(msg.sender, newVoterId);
+        registrations.push(msg.sender);
+        registered[msg.sender] = true;
+        voterRegistrationCard[msg.sender] = newVoterId;
+
+        return newVoterId;
+    }
+
+
+    function sendVoteToken() public {
+        require(registered[msg.sender] == true, "You have not registered to vote!");
+        require(balanceOf(msg.sender) < 1 , "You already have a VoteToken!");
+        require(!voted[msg.sender], "You have already recieved a VoteToken!");
+
+        increaseAllowance(msg.sender, 1);
+        transfer(msg.sender, 1);
     }
 
     function addCandidate(string memory candidateName) public restricted {
@@ -32,16 +67,19 @@ contract Voting{
         candidates[candidateCount] = candidate(candidateCount, candidateName, 0);
     }
     
-    function editCandidate(uint256 id, string memory candidateName) public restricted{
-        candidates[id] = candidate(id, candidateName, candidates[id].voteCount);
+    function editCandidate(uint256 id, string memory newCandidateName) public restricted{
+        candidates[id] = candidate(id, newCandidateName, candidates[id].voteCount);
     }
 
     function vote(uint candidateID) public {
-        require(msg.sender == ownerof(voterRegistrationCard[msg.sender]), "You do not have a valid VoterRegistrationCard!")
+        require(msg.sender == ownerOf(voterRegistrationCard[msg.sender]), "You do not have a valid VoterRegistrationCard!");
         require(!voted[msg.sender], "You have already voted!");
+        require(msg.sender != owner, "Owner can not vote!");
         require(candidateID > 0 && candidateID <= candidateCount, "Please select a valid candidate!");
+
         voted[msg.sender] = true;
         candidates[candidateID].voteCount++;
+        transferFrom(msg.sender, owner, 1);
 
         emit eventVote(candidateID);
     }
