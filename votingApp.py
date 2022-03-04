7a8d2f66d0e231ac4f02f90b685ed11ed2c07513
Raul Nogales <rulo.nogales@nogalesinvestments.com>
2022-03-04 05:46:25 +0100

    update function get the contract

--- votingApp.py
+++ votingApp.py
@@ -38,9 +38,9 @@ def load_contract():
 
     # Get the contract
     contract = w3.eth.contract(
-        address=contract_address,
-        abi=voting_abi
-    )
+    address = w3.toChecksumAddress(contract_address),
+    abi = voting_abi)
+
     # Return the contract from the function
     return contract
 
